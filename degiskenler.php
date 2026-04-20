2173c812b37c8bd1c7414fb6dc29bf0d7baebad4
Berat Aydoğan <berataydogan@ogrenci.beykoz.edu.tr>
2026-04-20 21:00:12 +0300

    Initial commit

---
+++ degiskenler.php
@@ -0,0 +1,18 @@
+<?php
+
+    $ad = "Berat";
+    $soyad = "Aydogan";
+
+    echo $ad." ".$soyad;
+    echo "<br>";
+
+    $sayi1= 100;
+    $sayi2= 400;
+    $sayi3 = $sayi1+$sayi2;
+    echo $sayi1 + $sayi2; //500
+    echo "<br>";
+    echo $sayi3; //500
+    echo "<br>";
+    echo $sayi1." ".$sayi2; //100 400
+
+?>
---
+++ ilk-uygulama.php
@@ -0,0 +1,18 @@
+<?php
+$title = "başlıktırha";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${title}</title>
+</head>
+<body>
+    <h1>    <?php echo $title;?>
+</h1>
+</body>
+</html>
