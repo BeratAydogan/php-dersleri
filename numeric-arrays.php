b4ae914b51fba655386e92dccc861e3d442af909
Berat Aydoğan <berataydogan@ogrenci.beykoz.edu.tr>
2026-04-21 23:41:06 +0300

    Numeric diziler

---
+++ numeric-arrays.php
@@ -0,0 +1,24 @@
+<?php
+
+    //Diziler
+        //Numeric Ddiziler
+        //Associative Diziler
+
+    //$students = "ali,ahmet,hasan"; YANLIS
+
+    //$students = array("ali","ahmet","hasan");
+    $students = ["ali","ahmet","hasan"];
+
+    echo $students[0]."<br>";
+    echo $students[1]."<br>";
+    echo $students[2]."<br>";
+    echo gettype($students)."<br>";
+
+    $numbers = [100,200,300];
+    echo $students[0]." ".$numbers[0]."<br>";
+    echo $students[1]." ".$numbers[1]."<br>";
+    echo $students[2]." ".$numbers[2]."<br>";
+
+
+
+?>
